b74254af1c505cc729d7f37578d1de01f1561bd8
gzavvo <gzavvo@gmail.com>
2021-02-22 16:37:46 +0200

    3.19: Phonebook database step7

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.delete('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const newPerson = request.body
 
   if (!newPerson.name) {
@@ -94,6 +94,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
