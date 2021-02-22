41a5a3ee7c71dc9c5e235b60de1a45c6eeb6d74a
gzavvo <gzavvo@gmail.com>
2021-02-22 11:23:28 +0200

    3.17: Phonebook database step5

--- index.js
+++ index.js
@@ -71,6 +71,20 @@ app.post('/api/persons', (request, response) => {
     .catch(error => next(error))
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updateNote => {
+      response.json(updateNote.toJSON())
+    })
+    .catch(error => next(error))
+})
+
 const errorHandler = (error, request, response, next) => {
   console.error('error message:', error.message)
   console.error('error name:', error.name)
